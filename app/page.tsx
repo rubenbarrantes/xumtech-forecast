f5f98c3db29615d4ca3e45ceaaca16ee404a3787
Ruben Barrantes <ruben.barrantes@xumtech.com>
2026-03-02 21:32:06 -0600

    fix ssr prerender + tribus roles tipos como params

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import XumtechSystem from "@/components/XumtechSystem";
+import dynamic from "next/dynamic";
+
+const XumtechSystem = dynamic(() => import("@/components/XumtechSystem"), { ssr: false });
 
 export default function Home() {
   return <XumtechSystem />;
