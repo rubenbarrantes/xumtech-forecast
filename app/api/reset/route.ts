52f2c464677b29b275431404f9018d029787e058
rubenbarrantes <ruben.barrantes@xum-tech.com>
2026-03-02 22:51:48 -0600

    Change GET to truncate only 'servicios' table
    Modified the GET function to truncate only the 'servicios' table instead of multiple tables.

--- app/api/reset/route.ts
+++ app/api/reset/route.ts
@@ -3,7 +3,7 @@ import sql from "@/lib/db";
 
 export async function GET() {
   try {
-    await sql`TRUNCATE TABLE colaboradores, servicios, asignaciones, ausencias, calendar, params RESTART IDENTITY CASCADE`;
+    await sql`TRUNCATE TABLE  servicios RESTART IDENTITY CASCADE`;
     return NextResponse.json({ ok: true, message: "Todas las tablas limpiadas correctamente" });
   } catch (error) {
     return NextResponse.json({ error: String(error) }, { status: 500 });
